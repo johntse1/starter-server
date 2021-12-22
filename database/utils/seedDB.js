ca1297ad094d8700c340bae54e50c03eca70413e
John Tse <jtse63@gmail.com>
2021-12-22 01:22:20 -0500

    fixed
    fixed the defaults entries

--- database/utils/seedDB.js
+++ database/utils/seedDB.js
@@ -3,16 +3,20 @@ const { Campus, Student } = require('../models');
 const seedDB = async () => {
 	const dummy_campus = await Campus.create({
 		name: "Hunter College",
-		description: "This is a school in NYC."
+		description: "This is a school in NYC.",
+		address: "21312312"
 	});
 	const dummy_campus2 = await Campus.create({
 		name: "Harvard",
-		description: "This is a school in MA."
+		description: "This is a school in MA.",
+		address: "2312231"
 	});
 
 	const dummy_student = await Student.create({
 			firstname: "Joe",
       lastname: "Shmo",
+	  email: "asfasf",
+	  gpa:1
 		});
 
 	await dummy_student.setCampus(dummy_campus);
